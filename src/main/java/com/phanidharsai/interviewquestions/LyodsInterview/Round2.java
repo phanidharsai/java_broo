723edaa55098842aacb0a144db2073f7a534230a
phanidharsai <phanidharsai111@gmail.com>
2023-09-01 13:53:52 +0530

    more programs

--- src/main/java/com/phanidharsai/interviewquestions/LyodsInterview/Round2.java
+++ src/main/java/com/phanidharsai/interviewquestions/LyodsInterview/Round2.java
@@ -6,32 +6,28 @@ public class Round2 {
     public static List<List<String>> getConnections(String[][] input, int num) {
         List<List<String>> res = new ArrayList<>();
         Map<String, Integer> temp = new HashMap<>();
-        List<String> keyList= new ArrayList<>();
         for (int i = 0; i < input.length; i++) {
-            if (input[i][0] == "CONNECT") {
+            if (input[i][0].equals("CONNECT")) {
                 if (!(temp.get(input[i][1]) == null && temp.get(input[i][2]) == null)) {
                     temp.put(input[i][1], temp.get(input[i][1]) + 1);
                     temp.put(input[i][2], temp.get(input[i][2]) + 1);
                 } else {
                     temp.put(input[i][1], 1);
                     temp.put(input[i][2], 1);
-                    keyList.add(input[i][1]);
-                    keyList.add(input[i][2]);
                 }
             } else {
                 temp.put(input[i][1], temp.get(input[i][1]) - 1);
                 temp.put(input[i][2], temp.get(input[i][2]) - 1);
             }
         }
-        System.out.println(keyList.toString());
         List<String> res0=new ArrayList<>();
         List<String> res1=new ArrayList<>();
-        for(int i=0;i< keyList.size();i++){
-            if(temp.get(keyList.get(i))>0){
-                res0.add(keyList.get(i));
+        for(Map.Entry<String,Integer> entry: temp.entrySet()){
+            if(entry.getValue()>0){
+                res0.add(entry.getKey());
             }
             else{
-                res1.add(keyList.get(i));
+                res1.add(entry.getKey());
             }
         }
         res.add(res0);
