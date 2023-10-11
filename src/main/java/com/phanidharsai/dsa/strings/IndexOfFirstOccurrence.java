7adaa480c80d1bd530911a4451d819843a23bf57
phanidharsai <phanidharsai111@gmail.com>
2023-10-11 12:28:25 +0530

    String dsa practice

--- src/main/java/com/phanidharsai/dsa/strings/IndexOfFirstOccurrence.java
+++ src/main/java/com/phanidharsai/dsa/strings/IndexOfFirstOccurrence.java
@@ -1,17 +1,37 @@
+/**  Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+ *   or -1 if needle is not part of haystack.
+ *  input: sadbutsad
+           sad
+    output: 0
+    input: leetcode
+           leeto
+    output: -1
+ * */
+
 package com.phanidharsai.dsa.strings;
 
 import java.util.Scanner;
 
 public class IndexOfFirstOccurrence {
-    public static void findIndex(String haystack, String needle){
+    public static int findIndex(String haystack, String needle) {
+        int lenOfNeedle=needle.length();
         if(!haystack.contains(needle)){
-            System.out.println("not found");
+            return -1;
         }
+        for(int i=0;i<haystack.length();i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                if(haystack.substring(i,i+lenOfNeedle).equals(needle)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String haystack= sc.nextLine();
         String needle= sc.nextLine();
-        findIndex(haystack,needle);
+        System.out.println(haystack.substring(6,9));
+        System.out.println(findIndex(haystack,needle));
     }
 }
