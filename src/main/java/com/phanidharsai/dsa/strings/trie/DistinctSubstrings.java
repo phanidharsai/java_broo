1eb129c234d3026fec55737bfaddba4c9b3552cc
phanidharsai <phanidharsai111@gmail.com>
2023-10-14 22:13:39 +0530

    String dsa practice

--- src/main/java/com/phanidharsai/dsa/strings/trie/DistinctSubstrings.java
+++ src/main/java/com/phanidharsai/dsa/strings/trie/DistinctSubstrings.java
@@ -18,8 +18,25 @@ public class DistinctSubstrings {
             }
             return res.size()+1;
         }
+        public static void getSol(char[] arr, int index, int len, List<Character> temp){
+            if(index==len){
+                return;
+            }
+            temp.add(arr[index]);
+            getDistinctSubstrings(arr,index+1,len,temp);
+            temp.remove(temp.size()-1);
+            getDistinctSubstrings(arr,index+1,len,temp);
+        }
+        public static int getDistinctSubstrings(char[] arr, int index, int len, List<Character> temp){
+            getSol(arr,index,len,temp);
+            return temp.size()+1;
+        }
         public static void main(String[] args){
             String s = "abc";
-            System.out.println(countDistinctSubstrings(s));
+            char[] arr= s.toCharArray();
+            List<Character> temp = new ArrayList<>();
+            int len = s.length();
+//            System.out.println(countDistinctSubstrings(s));
+            System.out.println(getDistinctSubstrings(arr,0,len,temp));
         }
     }
