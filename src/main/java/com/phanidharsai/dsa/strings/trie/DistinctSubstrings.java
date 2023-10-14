60641f07a62502ccd51c3aef5eb74c889d0dd3b1
phanidharsai <phanidharsai111@gmail.com>
2023-10-14 22:43:33 +0530

    String dsa practice

--- src/main/java/com/phanidharsai/dsa/strings/trie/DistinctSubstrings.java
+++ src/main/java/com/phanidharsai/dsa/strings/trie/DistinctSubstrings.java
@@ -1,42 +1,37 @@
 package com.phanidharsai.dsa.strings.trie;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DistinctSubstrings {
         public static int countDistinctSubstrings(String s)
         {
-            //	  Write your code here.
-            List<String> res= new ArrayList<>();
-            for(int i=0;i<=s.length();i++){
-                for(int j=i+1;j<=s.length();j++){
-                    if(!res.contains( s.substring(i,j))){
-                        res.add(s.substring(i,j));
-                        System.out.println(res);
+            Set<Character> charSet= new HashSet<>();
+            for(int i=0; i<s.length();i++){
+                charSet.add(s.charAt(i));
+            }
+            int len= charSet.size();
+            if(len==s.length()){
+                return 1+(len*(len+1))/2;
+            }
+            else{
+                List<String> res= new ArrayList<>();
+                for(int i=0;i<=s.length();i++){
+                    for(int j=i+1;j<=s.length();j++){
+                        if(!res.contains( s.substring(i,j))){
+                            res.add(s.substring(i,j));
+//                            System.out.println(res);
+                        }
                     }
                 }
+                return res.size()+1;
             }
-            return res.size()+1;
-        }
-        public static void getSol(char[] arr, int index, int len, List<Character> temp){
-            if(index==len){
-                return;
-            }
-            temp.add(arr[index]);
-            getDistinctSubstrings(arr,index+1,len,temp);
-            temp.remove(temp.size()-1);
-            getDistinctSubstrings(arr,index+1,len,temp);
-        }
-        public static int getDistinctSubstrings(char[] arr, int index, int len, List<Character> temp){
-            getSol(arr,index,len,temp);
-            return temp.size()+1;
         }
         public static void main(String[] args){
             String s = "abc";
-            char[] arr= s.toCharArray();
-            List<Character> temp = new ArrayList<>();
-            int len = s.length();
-//            System.out.println(countDistinctSubstrings(s));
-            System.out.println(getDistinctSubstrings(arr,0,len,temp));
+            System.out.println(countDistinctSubstrings(s));
+
         }
     }
