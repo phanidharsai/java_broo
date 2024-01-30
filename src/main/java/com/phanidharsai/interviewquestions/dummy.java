9b4c31cf61ef0beba2a58a7664c1205426c07276
phanidharsai <phanidharsai111@gmail.com>
2024-01-30 18:39:14 +0530

    refactoring

---
+++ src/main/java/com/phanidharsai/interviewquestions/dummy.java
@@ -0,0 +1,23 @@
+package com.phanidharsai.interviewquestions;
+
+public class dummy {
+        public static void main(String args[]) {
+            Bottom b = new Bottom();
+            b.stop();
+        }
+    }
+    class Top {
+    public int start() {
+        System.out.println("in top start");
+        return 0;
+    }
+}
+    class Bottom {
+    Top par = new Top();
+    public int stop() {
+        par.start();
+        System.out.println("in bottom stop");
+//        par.stop();
+        return 0;
+    }
+}
