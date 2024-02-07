d5eae89b6fa2635c386d187a4297189f5c7cfab1
phanidharsai <phanidharsai111@gmail.com>
2024-02-07 22:13:45 +0530

    more programs

--- src/main/java/com/phanidharsai/dsa/arrays/NextPermutation.java
+++ src/main/java/com/phanidharsai/dsa/arrays/NextPermutation.java
@@ -1,20 +1,31 @@
 package com.phanidharsai.dsa.arrays;
 
+import java.util.Arrays;
+
 public class NextPermutation {
+    public static void swap(int[] in,int a, int b){
+        int temp=in[a];
+        in[a]=in[b];
+        in[b]=temp;
+    }
     public static void nextPermutation(int[] nums) {
-        int min=nums[0];
-        int max=nums[nums.length-1];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<min){
-                min=nums[i];
-            }
-            if(nums[i]>max){
-                max=nums[i];
+        int ind =-1;
+        int n = nums.length;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
             }
         }
+        for(int j=n-1;j>=ind;j--){
+            if(nums[j]>nums[ind]){
+                swap(nums,ind,j);
+            }
+        }
+        System.out.println(Arrays.toString(nums));
     }
     public static void main(String[] args){
-        int[] nums={1,2,3};
+        int[] nums={1,3,2,4};
         nextPermutation(nums);
     }
 }
